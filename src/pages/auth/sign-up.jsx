c50812f6a21891eaf3a2211265270d5f14b0b6cd
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-12-24 13:19:15 +0100

    indentation

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -1,12 +1,10 @@
 import {
-  Card,
   Input,
   Checkbox,
   Button,
   Typography,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-
 
 export function SignUp() {
   return (
