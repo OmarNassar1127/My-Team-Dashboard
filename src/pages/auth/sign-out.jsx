65381e61a0a363483c5546611bf991e4b97e6e6c
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-12-24 10:02:14 +0100

    logout-handle

--- src/pages/auth/sign-out.jsx
+++ src/pages/auth/sign-out.jsx
@@ -1,40 +1,57 @@
-import React from 'react';
-import {
-  Card,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-} from "@material-tailwind/react";
+import React, { useState } from 'react';
+import { Dialog, Button, Typography } from "@material-tailwind/react";
 import { useLogout } from '../../api/useLogout';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 export function SignOut() {
+  const [open, setOpen] = useState(true); 
   const { logout, isLoading, error } = useLogout();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
-    const result = await logout();
-    if (result) {
-      navigate('/sign-in'); 
+    try {
+      await logout();
+      navigate('/auth/sign-in');
       window.location.reload();
+    } catch (e) {
+      console.error('Logout failed', e);
+      setOpen(false); 
     }
   };
 
+  const handleClose = () => {
+    setOpen(false);
+    navigate('/'); 
+  };
+
   return (
-    <section className="m-8 flex flex-col items-center justify-center">
-      <Typography variant="h5" className="mb-4">
-        Are you sure you want to sign out?
-      </Typography>
-      <Button 
-        onClick={handleLogout} 
-        disabled={isLoading}
-        color="red"
-      >
-        {isLoading ? 'Logging out...' : 'Sign Out'}
-      </Button>
-      {error && <Typography color="red">{error}</Typography>}
-    </section>
+    <Dialog size="sm" open={open} handler={handleClose}>
+      <div className="p-4">
+        <Typography variant="h5" className="mb-4">
+          Sign Out
+        </Typography>
+        <Typography variant="paragraph">
+          Are you sure you want to sign out?
+        </Typography>
+        <div className="mt-4 flex justify-end gap-2">
+          <Button 
+            color="blueGray" 
+            onClick={handleClose}
+            disabled={isLoading}
+          >
+            Cancel
+          </Button>
+          <Button 
+            color="red" 
+            onClick={handleLogout}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Logging out...' : 'Confirm'}
+          </Button>
+        </div>
+        {error && <Typography color="red">{error}</Typography>}
+      </div>
+    </Dialog>
   );
 }
 
