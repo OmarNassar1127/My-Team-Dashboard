84f82198a5ad69b6f4fdb69b8067d9173b2266cb
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-12-25 10:36:16 +0100

    Add useStoreManager hook and createManagerModal component

--- src/modals/createManagerModal.jsx
+++ src/modals/createManagerModal.jsx
@@ -2,18 +2,36 @@ import React, { useState } from "react";
 import {
   PlusIcon
 } from "@heroicons/react/24/solid";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useStoreManager } from '../api/useStoreManager'
 
 export default function CreateManagerModal() {
   const [showModal, setShowModal] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
+  const { storeManager, error } = useStoreManager();
+  const [managerData, setManagerData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    phone_number: '',
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setManagerData({ ...managerData, [name]: value });
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log({ name, email, password, phone });
-    setShowModal(false);
+    const { name, email, password, address, phone_number } = managerData;
+    try {
+      await storeManager({ name, email, password, address, phone_number });
+      setShowModal(false);
+      toast.success("Manager created successfully!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Error creating Manager. Please try again.");
+    }
   };
 
   return (
@@ -26,15 +44,23 @@ export default function CreateManagerModal() {
         <PlusIcon className="w-6 h-6 inline-block mr-2" />
         Create Manager
       </button>
+      <ToastContainer position="top-right"
+        autoClose={1750}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light" />
       {showModal ? (
         <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-md" style={{ width: '600px' }}>
-              {/* content */}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/* header */}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
                     Create Manager
@@ -48,14 +74,69 @@ export default function CreateManagerModal() {
                     </span>
                   </button>
                 </div>
-                {/* body */}
-                <form onSubmit={handleSubmit} className="relative p-6 flex flex-col">
-                  <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} className="mb-4 p-2 border rounded" />
-                  <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="mb-4 p-2 border rounded" />
-                  <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="mb-4 p-2 border rounded" />
-                  <input type="tel" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="mb-4 p-2 border rounded" />
+                <form onSubmit={handleSubmit} className="relative p-6 flex-auto">
+                  <div className="mb-4">
+                    <label htmlFor="name" className="mr-2 inline-block w-32 text-right">
+                      Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      onChange={handleInputChange}
+                      className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="address" className="mr-2 inline-block w-32 text-right">
+                      Email:
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      onChange={handleInputChange}
+                      className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="contact_info" className="mr-2 inline-block w-32 text-right">
+                      Password:
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      onChange={handleInputChange}
+                      className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="mr-2 inline-block w-32 text-right">
+                      Address:
+                    </label>
+                    <input
+                      type="address"
+                      id="address"
+                      name="address"
+                      onChange={handleInputChange}
+                      className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="mr-2 inline-block w-32 text-right">
+                      Phone:
+                    </label>
+                    <input
+                      type="text"
+                      id="phone_number"
+                      name="phone_number"
+                      onChange={handleInputChange}
+                      className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  {error && <p className="text-red-500">{error}</p>}
                 </form>
-                {/* footer */}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
                     className="bg-black text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
